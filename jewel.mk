79207ef6f85bf02242e05eed1bc70681dac1af49
andrew.boren <andrew.boren@gmail.com>
2012-07-11 18:59:56 -0700

    Fix dalvik heap config path.
    Change-Id: Iaf6c078b3ec1a29d3291e71adc0640128041decb

--- jewel.mk
+++ jewel.mk
@@ -232,7 +232,7 @@ PRODUCT_LOCALES += en_US
 $(call inherit-product-if-exists, vendor/htc/jewel/jewel-vendor.mk)
 
 # call dalvik heap config
-$(call inherit-product, frameworks/base/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
