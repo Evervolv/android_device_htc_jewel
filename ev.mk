c294eb7b77900ab21344ff6ed8992821b37e4267
kushdeck <kushdeck@gmail.com>
2012-10-13 07:47:26 -0700

    jewel: update fingerprint from stock 2.13 RUU
    Patchset2: remove dependency.
    
    Change-Id: I7fb32d9db247ec00bf96f2b9ca14a1f481bf118e

--- ev.mk
+++ ev.mk
@@ -15,7 +15,7 @@ PRODUCT_DEVICE := jewel
 PRODUCT_MODEL := htc_jewel
 PRODUCT_MANUFACTURER := HTC
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_jewel BUILD_ID=IML74K BUILD_FINGERPRINT=sprint_wwe/jewel/jewel:4.0.3/IML74K/60201.1:user/release-keys PRIVATE_BUILD_DESC="1.13.651.1 CL60201 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_jewel BUILD_ID=IMM76D BUILD_FINGERPRINT="sprint_wwe/jewel/jewel:4.0.4/IMM76D/99866.1:user/release-keys" PRIVATE_BUILD_DESC="2.13.651.1 CL99866 release-keys"
 
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := Bellus
