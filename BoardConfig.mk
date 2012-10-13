f88ffaf94b8649144b162a65948a374116bcf1d8
kushdeck <kushdeck@gmail.com>
2012-10-13 07:59:38 -0700

    jewel: update vold, storage_list, and ramdisk. add support for otg, usb hub.
    Patchset2: Rebased.
    
    Change-Id: Ic084d0586507a74d17eebc6467618000b9e1e40d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,6 +59,7 @@ WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/prima_wlan.ko"
 # Vold
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_VOLD_MAX_PARTITIONS := 38
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
 #dev:        size     erasesize name
 #mmcblk0p23: 000ffa00 00000200 "misc"
