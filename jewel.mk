81ab26db973d10fbdbeb4aa2c0d0f05cab5db079
andrew.boren <andrew.boren@gmail.com>
2012-11-03 18:56:20 -0700

    Fix jewel from breaking clean & clobber
    Change-Id: I0b208536f1a8305fed55036117e4db508fb648ad

--- jewel.mk
+++ jewel.mk
@@ -14,8 +14,6 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # common msm8960 configs
 $(call inherit-product, device/htc/msm8960-common/msm8960.mk)
 
@@ -143,5 +141,11 @@ PRODUCT_LOCALES += en_US
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/htc/jewel/jewel-vendor.mk)
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # call dalvik heap config
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+PRODUCT_NAME := htc_jewel
+PRODUCT_DEVICE := jewel
+PRODUCT_MODEL := Full Android on Jewel
