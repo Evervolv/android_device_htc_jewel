caf661c60fd6419f681ba63063ea9032fb74d6c6
Dorian Snyder <dastin1015@gmail.com>
2013-07-04 00:10:44 -0700

    jewel: bump to p3
    bump it up since we are using a new 3.4 msm8960 kernel :)
    
    Change-Id: I4e151708015a144d378bbcf490cbe857bc73f6e4

--- ev.mk
+++ ev.mk
@@ -19,7 +19,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_jewel BUILD_ID=JRO03C BUILD_FIN
 
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := Bellus
-PRODUCT_VERSION_DEVICE_SPECIFIC := p2
+PRODUCT_VERSION_DEVICE_SPECIFIC := p3
 
 PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your HTC Evo 4G LTE!\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
 
