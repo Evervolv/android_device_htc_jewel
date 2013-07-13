b61986e1489bdecf9148ce9176a10fafe06ec68d
Ethan Chen <intervigil@gmail.com>
2013-07-13 00:24:58 -0700

    jewel: mount firmware partitions
    Change-Id: I7b53397454dced3a08ee8136e8d2aa6b9915ab8c

--- jewel.mk
+++ jewel.mk
@@ -22,7 +22,8 @@ DEVICE_PACKAGE_OVERLAYS += device/htc/jewel/overlay
 # Boot ramdisk setup
 PRODUCT_PACKAGES += \
     fstab.qcom \
-    init.target.rc
+    init.target.rc \
+    remount.qcom
 
 # Sound configs
 PRODUCT_COPY_FILES += \
